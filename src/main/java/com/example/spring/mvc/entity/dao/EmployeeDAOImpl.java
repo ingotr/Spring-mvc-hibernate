1bd3f33d73855370d519fce5fd18c12f10218555
Aleksandr Markovkin <markovkin88@gmail.com>
2023-03-08 20:45:10 +0300

    7. feat: Добавить реализацию DAO

---
+++ src/main/java/com/example/spring/mvc/entity/dao/EmployeeDAOImpl.java
@@ -0,0 +1,30 @@
+package com.example.spring.mvc.entity.dao;
+
+import com.example.spring.mvc.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class)
+                .getResultList();
+        return allEmployees;
+    }
+
+    public EmployeeDAOImpl() {
+        super();
+    }
+}
