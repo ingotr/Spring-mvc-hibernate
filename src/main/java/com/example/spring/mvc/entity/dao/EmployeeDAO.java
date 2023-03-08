f207f94ed2c674919dec11d6609774f815165abb
Aleksandr Markovkin <markovkin88@gmail.com>
2023-03-08 20:44:49 +0300

    6. feat: Добавить DAO

---
+++ src/main/java/com/example/spring/mvc/entity/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package com.example.spring.mvc.entity.dao;
+
+import com.example.spring.mvc.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+}
